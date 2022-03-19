270a69bf2537a246d4a4cb346c317113971062d5
Juanchi <gallegojpe@gmail.com>
2022-03-19 03:35:34 -0300

    fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,34 +10,37 @@ import '../Styles/Header.scss'
 function Header() {
     return (
         <header>
-            <div className="adress__container">
-                <LocationOnIcon className='header__icon' />
-                <div className="adress__data">
-                    <p>Enviar a JUAN</p>
-                    <strong>Juramento 4993</strong>
+            <div className="header__content">
+                <div className="adress__container">
+                    <LocationOnIcon className='header__icon' />
+                    <div className="adress__data">
+                        <p>Enviar a JUAN</p>
+                        <strong>Juramento 4993</strong>
+                    </div>
+                </div>
+                <ul className='header__navegation'>
+                    <li>
+                        Categorias <KeyboardArrowDownIcon />
+                    </li>
+                    <li>Ofertas</li>
+                    <li>Historial</li>
+                    <li>Supermercado</li>
+                    <li>Moda</li>
+                    <li>Vender</li>
+                    <li>Ayuda</li>
+                </ul>
+
+                <div >
+                    <ul className="user__options">
+                        <li><AccountCircleIcon className='user__options__icon' /> JUAN PEDRO  <KeyboardArrowDownIcon className='user__options__icon' /></li>
+                        <li>Mis Compras</li>
+                        <li>Favoritos<KeyboardArrowDownIcon className='user__options__icon' /> </li>
+                        <li><NotificationsIcon className='user__options__icon' /></li>
+                        <li><ShoppingCartOutlinedIcon className='user__options__icon' /></li>
+                    </ul>
                 </div>
             </div>
-            <ul className='header__navegation'>
-                <li>
-                    Categorias <KeyboardArrowDownIcon />
-                </li>
-                <li>Ofertas</li>
-                <li>Historial</li>
-                <li>Supermercado</li>
-                <li>Moda</li>
-                <li>Vender</li>
-                <li>Ayuda</li>
-            </ul>
 
-            <div >
-                <ul className="user__options">
-                    <li><AccountCircleIcon className='user__options__icon' /> JUAN PEDRO  <KeyboardArrowDownIcon className='user__options__icon' /></li>
-                    <li>Mis Compras</li>
-                    <li>Favoritos<KeyboardArrowDownIcon className='user__options__icon' /> </li>
-                    <li><NotificationsIcon className='user__options__icon' /></li>
-                    <li><ShoppingCartOutlinedIcon className='user__options__icon' /></li>
-                </ul>
-            </div>
         </header>
     )
 }
